4c16cd0eea34cba1e87d3356ba5fe5348c62c928
Tevin Deja <tevinowena@gmail.com>
2023-09-16 06:49:52 -0700

    _putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - write the character of c
  * 0c: The characer is printed
@@ -7,7 +7,7 @@
  * Return: 1 success
  * On error -1 is returned
  */
-int putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
