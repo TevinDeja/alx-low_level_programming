341b3f6f0d5499fdf596e42a79207f8120347600
Tevin Deja <tevinowena@gmail.com>
2023-09-16 07:59:39 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
- *
+ * @n: parameter to be checked
  * Return: 1 if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
@@ -12,8 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
