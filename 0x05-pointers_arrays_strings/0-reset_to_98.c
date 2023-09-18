4a58270e3e6dec1d9eedfabe4a2a2d56c93e1663
Tevin Deja <tevinowena@gmail.com>
2023-09-18 01:15:16 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
+ * reset_to_98 - function that takes a ponter to an int
+ * @n: input parameter
  *
- *
- *
- *
+ * Return: n
  */
 
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
