b150554f548b8c1ec7bd98308cb3831b295fa0f3
Tevin Deja <tevinowena@gmail.com>
2023-09-24 23:10:23 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * memcpy - function that copies memory area
+ * @dest: storage memory
+ * @src: source memory
+ * @n: int value
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int m;
+	int p = n;
+
+	m = 0;
+	for ( ; m < p ; m++)
+	{
+		dest[m] = src[m];
+		n--;
+	}
+	return (dest);
+}
