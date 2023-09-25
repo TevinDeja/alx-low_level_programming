228f4ab4d90eac86eb3913a10bf348ab6b4e2361
Tevin Deja <tevinowena@gmail.com>
2023-09-25 04:32:31 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character of c
+ * @c: The characer is printed
+ *
+ * Return: 1 success
+ * On error -1 is returned
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
