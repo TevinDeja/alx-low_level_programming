14a90b1f711484887aa6497ccdf89421bdf64a92
Tevin Deja <tevinowena@gmail.com>
2023-09-24 22:57:34 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * memset - function that fills memory with a constant byte
+ * @s: string value
+ * @b: char value
+ * @n: int value
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int m;
+
+	m = 0;
+	for ( ; n > 0 ; m++)
+	{
+		s[m] = b;
+		n--;
+	}
+	return (s);
+}
