2b1cceddf948e7eabc94eeb656026dcd214e2c2f
Tevin Deja <tevinowena@gmail.com>
2023-10-02 03:19:21 -0700

    Main commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates array of chars and initializes it
+ * @size:
+ * @c:
+ *
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+C - malloc, free
+C Memory allocation
+4 Mandatory tasks 2 Advanced
