fe7c07f2074cf1e8e5b9003f4154b17a4beb0b1b
Tevin Deja <tevinowena@gmail.com>
2023-10-03 07:55:51 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - C program that prints exactly the given word.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  *  main - C program that prints the size of various types
  *
  *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char a;
