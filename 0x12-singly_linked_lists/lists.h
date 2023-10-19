4e5ca9bb4d25d4a840060b1b9a98bd452dda0302
Tevin Deja <tevinowena@gmail.com>
2023-10-19 11:39:56 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,27 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string pointer
+ * @len: string length
+ * @next: pointer
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
