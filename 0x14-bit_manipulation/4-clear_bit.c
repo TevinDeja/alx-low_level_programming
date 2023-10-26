3837657bac7ad25b7a69270908d209bdaf0eeb10
Tevin Deja <tevinowena@gmail.com>
2023-10-26 09:29:05 -0700

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets value of a bit to 0 at a given index
+ * @n: bits checker
+ * @index: index of bits
+ *
+ * Return: 1 if it worked or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
