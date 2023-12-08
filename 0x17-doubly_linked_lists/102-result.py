887af12603b44d98735b83d7cb4f59a7446448bb
Tevin Deja <tevinowena@gmail.com>
2023-12-08 09:15:37 -0800

    Palindromes

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-def is_palindrome(num):
-    return str(num) == str(num)[::-1]
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
 
-largest_palindrome = 0
+def largest_3digit_palindrome():
+    max_palindrome = 0
+    for x in range(100, 1000):
+        for y in range(100, 1000):
+            product = x * y
+            if is_palindrome(product) and product > max_palindrome:
+                max_palindrome = product
+    return max_palindrome
 
-for i in range(100, 1000):
-    for j in range(100, 1000):
-        product = i * j
-        if is_palindrome(product) and product > largest_palindrome:
-            largest_palindrome = product
-with open("102-result", "w") as file:
-    file.write(str(largest_palindrome))
+result = largest_3digit_palindrome()  
+print(result)
