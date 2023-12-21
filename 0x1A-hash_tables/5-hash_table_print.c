30fcf10c5e3fcd7c969feb8d1dcde41746640fa8
Tevin Deja <tevinowena@gmail.com>
2023-12-21 02:42:11 -0800

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: A pointer to the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	char *comma = "";
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				printf("%s'%s': '%s'", comma, node->key, node->value);
+				comma = ", ";
+				node = node->next;
+			}
+		}
+	}
+	printf("}\n");
+}
